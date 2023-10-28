95d30032dd19da0345bfbc1f58ae361b5e6ac2a7
ikram9820 <ikram9820khan@gmail.com>
2023-10-28 12:54:54 +0500

    functionalities completed

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
-import { Fragment } from "react";
 import "./App.css";
-import Header from "./components/Header";
-import Nav from "./components/Nav";
-import Main from "./components/Main";
+import Navbar from "./components/Navbar";
+import Routing from "./components/Routing";
 import Footer from "./components/Footer";
-
 function App() {
   return (
-    <Fragment className="App">
-      <Nav />
-      <Header />
-      <Main />
+    <>
+      <Navbar />
+      <Routing />
       <Footer />
-    </Fragment>
+    </>
   );
 }
 
